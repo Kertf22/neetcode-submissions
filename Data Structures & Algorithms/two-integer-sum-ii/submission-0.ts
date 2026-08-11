26d3db039877d930dd82c5cbcc50a0c850e94d6a
Paulo Hercílio Araújo Santana <70054149+Kertf22@users.noreply.github.com>
2026-08-11 15:06:20 -0300

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+
+        let left=0;
+        let right=numbers.length-1;
+
+        while(right>left) {
+            let sum = numbers[left] + numbers[right];
+            if (sum==target) return [left+1,right+1];
+            else if (sum > target) right--;
+            else left++;
+        }
+
+        return null;
+    }
+}
