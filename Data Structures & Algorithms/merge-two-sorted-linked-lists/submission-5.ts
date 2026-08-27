87b6252cf8d26902f86c9200be17f8cc1e91d41c
Paulo Hercílio Araújo Santana <70054149+Kertf22@users.noreply.github.com>
2026-08-27 00:33:21 -0300

    Add: merge-two-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-5.ts
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode {
+       if (!list1 && !list2) return null;
+        if (list1 && !list2) return list1;
+        if (!list1 && list2) return list2;
+        let pos1 = list1;
+        let pos2 = list2;
+        let res:any = {};
+        let current = res;
+        while(pos1 || pos2) {
+            let minPos = null;
+            if(!pos1 && pos2) minPos=1
+            else if(pos1 && !pos2) minPos=0
+            else if(pos1 && pos2 && pos1.val > pos2.val) minPos=1
+            else minPos=0;
+            if(current.val == undefined) current.val = minPos ? pos2.val : pos1.val;
+            else {
+            current.next = {
+                val: minPos ? pos2.val : pos1.val,
+                next: null,
+            }
+            current = current.next;
+            } 
+            if(minPos) pos2=pos2.next;
+            else pos1=pos1.next;
+        }
+        console.log(JSON.stringify(res))
+        return res;
+    }
+}
